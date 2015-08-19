ee7572a16ca41ea0ad1f8995feaafaa617388fb9
Satish Yadav <monstersatish@live.com>
2015-08-19 18:32:28 +0530

    Init

---
+++ BirthdayReminder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BirthdayReminder.Startup))]
+namespace BirthdayReminder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
